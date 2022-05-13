30ac9374b9f61ad5d051ce2e4cc93021b7f309d5
pabloamtzs <a01252489@itesm.mx>
2022-05-12 17:36:49 -0700

    reordenamiento de archivos

---
+++ acts/prueba.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	char nombre[20];
+	printf("Nombre: ");
+	scanf("%s", nombre);
+	printf("\nHola %s\n", nombre);
+	return 0;
+}
+
---
+++ acts/prueba.py
@@ -0,0 +1,3 @@
+nombre =  input("Nombre: ")
+print(f"Hola {nombre} ")
+
